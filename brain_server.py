478ed461f93aaabd4c7cba966d44098c62d4661e
xsmbv23 <data@vinatea.vn>
2026-08-17 07:46:08 +0700

    foundation: enforce boot-time verifier gate before Brain server

--- brain_server.py
+++ brain_server.py
@@ -2,6 +2,10 @@ from brain.server import Handler
 from http.server import HTTPServer
 import os
 
+from tools.runtime_boot_gate import main as run_foundation_boot_gate
+
 
 if __name__ == "__main__":
+    if run_foundation_boot_gate() != 0:
+        raise SystemExit("FOUNDATION_BOOT_GATE_DENY")
     HTTPServer(("0.0.0.0", int(os.environ.get("PORT", "10000"))), Handler).serve_forever()
