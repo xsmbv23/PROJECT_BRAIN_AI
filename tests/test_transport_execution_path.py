e9ea2bc8e6f6e1f1cad0ce4f1e664234dc0d989b
xsmbv23 <data@vinatea.vn>
2026-08-20 01:11:06 +0700

    foundation: correct authenticated transport path test

--- tests/test_transport_execution_path.py
+++ tests/test_transport_execution_path.py
@@ -4,16 +4,41 @@ import unittest
 from brain.server import Handler
 
 
+class _Headers:
+    def __init__(self, token=""):
+        self.token = token
+
+    def get(self, key, default=""):
+        if key == "X-Forensic-Probe-Token":
+            return self.token
+        return default
+
+
+class _Request:
+    def __init__(self, token=""):
+        self.headers = _Headers(token)
+
+
 class TransportExecutionPathTests(unittest.TestCase):
     def test_probe_token_is_required(self):
         old = os.environ.get("FORENSIC_PROBE_TOKEN")
         try:
             os.environ["FORENSIC_PROBE_TOKEN"] = "secret"
-            self.assertFalse(Handler._probe_authorized)
+            self.assertFalse(Handler._probe_authorized(_Request("")))
+            self.assertFalse(Handler._probe_authorized(_Request("wrong")))
+            self.assertTrue(Handler._probe_authorized(_Request("secret")))
         finally:
             if old is None:
                 os.environ.pop("FORENSIC_PROBE_TOKEN", None)
             else:
+                os.environ["FORENSIC_PROBE_TOKEN"] = old
+
+    def test_missing_server_token_denies(self):
+        old = os.environ.pop("FORENSIC_PROBE_TOKEN", None)
+        try:
+            self.assertFalse(Handler._probe_authorized(_Request("anything")))
+        finally:
+            if old is not None:
                 os.environ["FORENSIC_PROBE_TOKEN"] = old
 
 
